c0d4471e89c67ec2cf98c0b9cb05c286245224a9
Louie-091107 <johnlouiecalusa@gmail.com>
2025-03-21 06:51:11 +0800

    Add files via upload

---
+++ about.js
@@ -0,0 +1,66 @@
+//step 1: get DOM
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function(){
+    showSlider('next');    
+}
+
+prevDom.onclick = function(){
+    showSlider('prev');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+    
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}
+
+// App Gallery Carousel Functionality
+const appList = document.querySelector(".app-list");
+const appPrev = document.getElementById("app-prev");
+const appNext = document.getElementById("app-next");
+
+let appIndex = 0;
+
+appNext.addEventListener("click", () => {
+    appIndex = (appIndex + 1) % 4; // Adjust if adding more items
+    appList.style.transform = `translateX(-${appIndex * 100}%)`;
+});
+
+appPrev.addEventListener("click", () => {
+    appIndex = (appIndex - 1 + 4) % 4;
+    appList.style.transform = `translateX(-${appIndex * 100}%)`;
+});
